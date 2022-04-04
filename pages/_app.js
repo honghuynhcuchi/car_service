2e9ee8ff09ec7a8e7cc5200cdf19adf2ce08d99a
Phi Hong <honghuynhcuchi123@gmail.com>
2022-04-04 23:46:49 +0700

    set up project

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
