0c6334dd8f93c9d560493bc03a36852049c3a2a2
honghuynhcuchi <honghuynhcuchi123@gmail.com>
2022-04-10 10:53:25 +0700

    update business page

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -6,31 +6,33 @@ const Footer = () => {
     <div className={styles["footer-wrapper"]}>
       <div className={styles["container"]}>
         <Row>
-          <Col md={2} className={styles['footer-section']}>
-            <a href="#">
-              <img src="/images/logo_white.png" />
-            </a>
+          <Col md={2} className={styles["footer-section"]}>
+            <Link href={"/"}>
+              <a>
+                <img src="/images/logo_white.png" />
+              </a>
+            </Link>
           </Col>
-          <Col md={2} className={styles['footer-section']}>
+          <Col md={2} className={styles["footer-section"]}>
             <div className={styles["content-wrapper"]}>
               <Link href="/">
-              <a>
-                <h5 className={styles["content"]}>Giới thiệu</h5>
-              </a>
+                <a>
+                  <h5 className={styles["content"]}>Giới thiệu</h5>
+                </a>
               </Link>
               <Link href="/booking">
-              <a >
-                <h5 className={styles["content"]}>Đặt lịch hẹn</h5>
-              </a>
+                <a>
+                  <h5 className={styles["content"]}>Đặt lịch hẹn</h5>
+                </a>
               </Link>
               <Link href="/business">
-              <a >
-                <h5 className={styles["content"]}>Quy trình dịch vụ</h5>
-              </a>
+                <a>
+                  <h5 className={styles["content"]}>Quy trình dịch vụ</h5>
+                </a>
               </Link>
             </div>
           </Col>
-          <Col md={8} className={styles['footer-section']}>
+          <Col md={8} className={styles["footer-section"]}>
             <div className={styles["content-wrapper"]}>
               <h5 className={styles["content"]}>Liên hệ với chúng tôi</h5>
               <div>
